c9d2a7baaca00a348b0534f056d0d8cf02d607f3
lightwaver <lightwaver@gmx.at>
2017-11-05 13:24:38 +0100

    NuSpec File

--- HtmlLabel/Properties/AssemblyInfo.cs
+++ HtmlLabel/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HtmlLabel")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Xamarin.Forms Control for showing simple HTML Text in a Label")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HtmlLabel")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Marekworks.at - Stefan M. Marek")]
+[assembly: AssemblyProduct("MWX.XamForms.HtmlLabel")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
