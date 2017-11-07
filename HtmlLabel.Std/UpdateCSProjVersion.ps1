b89d78049bfe67c9bdaa945a354401b1af9ea2e2
lightwaver <lightwaver@gmx.at>
2017-11-07 21:08:05 +0100

    .Net Standard mit VersionUpdate Script

---
+++ HtmlLabel.Std/UpdateCSProjVersion.ps1
@@ -0,0 +1,24 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True)]
+    [String]$version
+)
+#WriteInfomation(Get-ScriptDirectory)
+
+$manifestPath = $(Get-Location).Path
+$manifestPath = $manifestPath + "\HtmlLabel.Std.csproj"
+
+$appConfig = New-Object XML
+
+# make a backup
+Copy-Item -Path $manifestPath -Destination $($manifestPath + ".bak") -Force
+
+# load the config file as an xml object
+$appConfig.Load($manifestPath)
+
+    
+Write-Host $("mapping VersionCode " + $appConfig.Project.PropertyGroup.Version + " to " + $version)
+
+$appConfig.Project.PropertyGroup.Version = $version
+
+$appConfig.Save($manifestPath)
